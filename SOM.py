2eedbfd18370cd68d72258adfe8eaa1425b4e188
Franco Aquistapace <tatoaquista@gmail.com>
2021-09-09 14:52:52 -0300

    Add files via upload

---
+++ SOM.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pandas as pd
+
+
+#This function is the learning rate
+# @param t expects an int or float value
+# @param max_eta expects a float value as the maximum learning rate
+def eta(max_eta, t):
+    return max_eta/t
+
+#Function for the Gaussian sigma
+def sigma(max_sigma, t):
+    return max_sigma/t
+
+# @params x,w_m expect 1D arrays
+# @out returns euclidean distance of x-w_m
+def Euclidean_dist(x,w_m):
+    d_vec = x - w_m
+    D = np.sqrt(np.dot(d_vec,d_vec))
+    return D
+
+# @param sigma_0 expects the max_sigma of the network
+# @param t expects a given iteration step
+# @param D expects the values of the output nodes
+# @param d_min expects the index of the BMU
+def h(sigma_0,t,D,d_min):
+    I = np.ones(np.shape(D))
+    d = np.power((D-I*D[d_min]), 2)
+    coef = -1/(2*sigma(sigma_0, t)**2)
+    result = np.exp(d*coef)
+    return result
+
+
+class SOM(object):
+    def __init__(self, sigma, eta, size):
+        #parameters
+        self.sigma = sigma
+        self.eta = eta
+        # @param size expects a two tuple with the input
+        # and output number of neurons
+        self.inputLayerSize = size[0]   # X1, X2, X3, ...
+        self.outputLayerSize = size[1]  # Y1, Y2, Y3, ...
+        
+        # build weights of each layer, set to random values
+        # look at the interconnection diagram to make sense of this
+        # 3x4 matrix for input to output
+        self.W1 = \
+                np.random.rand(self.inputLayerSize, self.outputLayerSize)
+                
+    #This function finds the Best Matching Unit (BMU)
+    # @param x expects the input layer
+    # @param W expects the weight matrix
+    def find_nodes_and_BMU(self,x):
+        #Calculate distances
+        d = []
+        for m in range(self.outputLayerSize):
+            d.append(Euclidean_dist(x,self.W1[:,m]))
+        D = np.array(d, dtype=float)
+        m = np.amin(D)
+        #Return BMU
+        return D, d.index(m)
+    
+    
+    #This function corrects the weights in W1
+    def update(self,x,t):
+        D, d_min = self.find_nodes_and_BMU(x)
+        m = self.outputLayerSize
+        H = np.ones((m,m))*h(self.sigma, t, D, d_min)
+        X = x.reshape((self.inputLayerSize,1))*np.ones((self.inputLayerSize,m))
+        new_W1 = self.W1 + eta(self.eta,t)*np.matmul((X-self.W1), H)
+        self.W1 = new_W1
+        
+    def save_weights(self):
+        # save this in order to reproduce our cool network
+        np.savetxt("weights.txt", self.W1, fmt="%s")
+    
+    #This function predicts an individual output based on an input x and trained weights
+    #Returns the index of the BMU
+    def predict_output(self,x):
+        D, d_min = self.find_nodes_and_BMU(x)
+        return d_min
+    
+    
+    
+    #Now lets define the training process
+    def train(self, data):
+        # @param data expects an ndarray or DataFrame
+        if str(type(data)) == '<class \'numpy.ndarray\'>':
+            n = data.shape[0]
+            for i in range(n):
+                self.update(data[i,:],i+1)
+                
+        elif str(type(data)) == '<class \'pandas.core.frame.DataFrame\'>':
+            n = data.shape[0]
+            new_data = data.to_numpy()
+            for i in range(n):
+                self.update(new_data[i,:],i+1)
+            
+        
+    def predict(self, data):
+        # @param data expects an ndarray or a DataFrame
+        if str(type(data)) == '<class \'numpy.ndarray\'>':
+            n = data.shape[0]
+            out_values = np.zeros((n,1))
+            for i in range(n):
+                output = self.predict_output(data[i,:])
+                out_values[i,0] = output
+            result = np.hstack((data,out_values))
+            return result
+                
+        elif str(type(data)) == '<class \'pandas.core.frame.DataFrame\'>':
+            n = data.shape[0]
+            new_data = data.to_numpy()
+            out_values = np.zeros((n,1))
+            for i in range(n):
+                output = self.predict_output(new_data[i,:])
+                out_values[i,0] = output
+            np_result = np.hstack((new_data,out_values))
+            result = pd.DataFrame(data = np_result)
+            return result    
---
+++ main.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 11:01:47 2021
+
+@author: francoaquistapace
+"""
+
+# Import modules and SOM
+import pandas as pd
+
+from SOM import *
+
+# Open file and extract column names
+path = input('Insert file path: ', )
+print('Reading file...')
+#path = 'dump.ensayo.2900000.config'
+file = open(path,'r')
+found_cols = False
+header_lines = []
+line_count = 0
+while not found_cols:
+    line = file.readline()
+    line_count += 1
+    if 'ITEM: ATOMS' in line:
+        columns = line.split()
+        columns.pop(0) # Pop 'ITEM:'
+        columns.pop(0) # Pop 'ATOMS'
+        found_cols = True
+    header_lines.append(line)
+file.close()
+
+# Build DataFrame from data file
+print('Importing data...')
+df = pd.read_csv(path,sep=' ', 
+                 skiprows=line_count, names=columns)
+
+# Show columns so that the user can select the features
+print('\nAvailable columns: ')
+print(columns)
+
+
+# Select features, could improve on this in the future
+features = []
+message = 'Insert a feature name to include, \'exit\' to quit the program' +\
+         ' or press enter to continue: '
+print('\n(If no feature is selected, '+\
+      'all available columns are taken as features)\n')
+key = input(message, )
+while key != '':
+    # Check if user wants to exit
+    if key == 'exit':
+        print('Process terminated')
+        exit()
+        
+    # Check that the input matches one of the column names and that
+    # it hasn't already been added to the features
+    if key in columns and not key in features:
+        features.append(key)
+    else:
+        print('Error: Feature is already selected or not available')
+    
+    key = input(message, )
+
+# If the user pressed enter directly we use all the columns as features
+if len(features) == 0:
+    features = columns
+    print('\nUsing all columns as features\n')
+else:
+    print('\nUsing features: %s\n' % features)
+
+# Normalize features
+norm_df = df[features].copy()
+for feat in features:
+    norm_df[feat] = norm_df[feat]/norm_df[feat].max()
+    
+# Shuffle for training
+frac = input('Insert fraction of the data to be used for '+\
+             'training, must be between 0 and 1: ',)
+if frac == '1':
+    frac = int(1)
+else:
+    frac = float(frac)
+training_df = norm_df.sample(frac=frac)
+
+
+# Initialize SOM parameters
+print('\nPredetermined SOM parameters are:' + '\nSIGMA = 1' + '\nETA = 0.5')
+SIGMA = input('Insert new SIGMA parameter or press enter to pass: ',)
+if SIGMA == '':
+    SIGMA = 1
+else:
+    SIGMA = float(SIGMA)
+ETA = input('Insert new ETA parameter or press enter to pass: ',)
+if ETA == '':
+    ETA = 0.5
+else:
+    ETA = float(ETA)
+    
+GROUPS = int(input('Insert number of clusters: ',))
+SIZE = (len(features),GROUPS)
+# Build SOM model
+som = SOM(sigma=SIGMA, eta=ETA, size=SIZE)
+
+
+# Train SOM and predict groups
+print('\nTraining SOM...')
+som.train(training_df)
+print('Predicting atom groups...')
+results = som.predict(norm_df)
+# We only need the last column, which contains the grouping result
+result_cols = results.columns.to_list()
+groups = results[result_cols[-1]]
+
+# Concat new DataFrame
+new_df = pd.concat([df,groups], axis=1)
+
+
+# Save new file with the group assigned to each atom
+print('Writing results...')
+new_path = 'SOM_' + path
+new_file = open(new_path, 'w')
+
+# Write the header of the file
+for i in range(len(header_lines)):
+    line = header_lines[i]
+    if i == len(header_lines) - 1:
+        line = line.replace('\n', ' cluster\n')
+    new_file.write(line)
+
+# Now let's write the new data
+# First we specifiy the format of each line
+formatting = ''
+for i in range(new_df.shape[1]):
+    if i == new_df.shape[1]-1:
+        formatting += '%d\n'
+    else:
+        formatting += '%s '
+# Then we write the lines
+for i in range(new_df.shape[0]):
+    new_line_tuple = tuple(new_df.iloc[i].to_list())
+    # Write new line
+    new_file.write(formatting % new_line_tuple)
+    
+new_file.close()
+print('Process completed')
+    
+
