66af4a377a0c8f3b8145ca414466d76211fc6dc8
Franco Aquistapace <tatoaquista@gmail.com>
2021-09-25 17:00:44 -0300

    Update SOM.py

--- SOM.py
+++ SOM.py
@@ -1,5 +1,5 @@
 """
-Copyright [2021] [Franco Aquistapace]
+Copyright 2021 Franco Aquistapace
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
